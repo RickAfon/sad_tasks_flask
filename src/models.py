4a4e30a25f5db91c5bda2a1581eb988fefa47edd
Chufretalas <marcobene.contact@gmail.com>
2022-09-27 17:15:29 -0300

    did the basic structure for the first view

---
+++ src/models.py
@@ -0,0 +1,10 @@
+from dataclasses import dataclass
+from datetime import datetime
+from typing import Optional
+
+@dataclass
+class Task():
+    title: str
+    tag: Optional[str]
+    content: Optional[str]
+    creation_date: datetime
--- src/views.py
+++ src/views.py
@@ -1,6 +1,28 @@
+from datetime import datetime
 from flask import render_template
 from app import app
+from models import Task
 
 @app.route("/")
 def index():
-    return render_template("index.html")
+    tasks = [
+        Task('Do something','School', '''Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas facilisis sed sapien eget
+                        lacinia. Aenean enim est, laoreet eu mauris vel, pharetra elementum leo. Sed pellentesque eget
+                        mi vitae imperdiet. Donec nec odio leo. Aliquam interdum at orci id tincidunt. Maecenas ante
+                        ligula, dignissim et finibus a, malesuada ac ex.''', datetime.now()),
+        Task('Do something','Work', '''Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas facilisis sed sapien eget
+                        lacinia. Aenean enim est, laoreet eu mauris vel, pharetra elementum leo. Sed pellentesque eget
+                        mi vitae imperdiet. Donec nec odio leo. Aliquam interdum at orci id tincidunt. Maecenas ante
+                        ligula, dignissim et finibus a, malesuada ac ex.''', datetime.now()),
+        Task('Do something', None, '''Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas facilisis sed sapien eget
+                        lacinia. Aenean enim est, laoreet eu mauris vel, pharetra elementum leo. Sed pellentesque eget
+                        mi vitae imperdiet. Donec nec odio leo. Aliquam interdum at orci id tincidunt. Maecenas ante
+                        ligula, dignissim et finibus a, malesuada ac ex.''', datetime.now()),
+        Task('Do something','School', '''Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas facilisis sed sapien eget
+                        lacinia. Aenean enim est, laoreet eu mauris vel, pharetra elementum leo. Sed pellentesque eget
+                        mi vitae imperdiet. Donec nec odio leo. Aliquam interdum at orci id tincidunt. Maecenas ante
+                        ligula, dignissim et finibus a, malesuada ac ex.''', datetime.now()),
+        Task('Do something','School', None, datetime.now()),
+        Task('Do something',None, None, datetime.now())
+    ]
+    return render_template("index.html", tasks=tasks)
